166aa5bb58cf2555b05834cefc8cd050fd2eb055
Pilate Chinyengetere <pilatech6@gmail.com>
2022-12-09 03:43:30 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,8 @@ int system(const char *command);
  **/
 int main(void)
 {
-int (*sys)(char*) = system;
+int (*sys)(char *)
+sys = system;
 
 sys(">&2 echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
 return (1);
