554786a440506ecee357b3d0674f314df93db6e6
Pilate Chinyengetere <pilatech6@gmail.com>
2022-12-08 18:05:42 -0800

    add printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using printf
+ * Return: whether success or not
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
