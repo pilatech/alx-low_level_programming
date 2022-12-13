4ddd7e9a7b43119650eed9284f79733d663cef07
Pilate Chinyengetere <pilatech6@gmail.com>
2022-12-13 03:02:34 -0800

    add print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - to print lower and upper case alphabet
+ * Return: 0 if successful otherwise nonzero
+ */
+int main(void)
+{
+int ch = 97;
+
+while (ch <= 122)
+{
+	putchar(ch);
+	ch++;
+}
+ch = 65;
+
+while (ch <= 90)
+{
+	putchar(ch);
+	ch++;
+}
+putchar(10);
+return (0);
+}
