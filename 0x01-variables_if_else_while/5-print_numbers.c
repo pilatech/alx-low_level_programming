a4f823fb122961d3423103efc8f724ebd4e1a35a
Pilate Chinyengetere <pilatech6@gmail.com>
2022-12-13 03:31:37 -0800

    add print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - to print 0 - 9
+ * Return: 0 if successful otherwise nonzero
+ */
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+	printf("%i", n);
+	n++;
+}
+printf("\n");
+return (0);
+}
