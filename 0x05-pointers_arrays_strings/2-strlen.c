4ed5448a2a4112d4131107d43a4c0f1cac9cbfb0
Pilate Chinyengetere <pilatech6@gmail.com>
2022-12-19 18:22:50 -0800

    add _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - find the length of a string
+ * Return: an integer of count
+ * @s: the string pointer
+ */
+
+int _strlen(char *s)
+{
+ int count = 0;
+
+ while (*s != 0)
+ {
+	 count++;
+	 s++;
+ }
+ return (count);
+}
