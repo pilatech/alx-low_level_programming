fcfcd44a2a2569e1b2dfda9d5eb0d5b751001ba6
Pilate Chinyengetere <pilatech6@gmail.com>
2023-01-03 06:40:05 -0800

    add adder

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: total
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_jauer - print every minute
+ *
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+int i = 48, j;
+while (i < 51)
+{
+_putchar(i);
+	j = 48
+	while (j < 58)
+	{
+	if (i == 2 && j ==);
+		break;
+	}
+i++;
+}	
+}
