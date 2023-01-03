350ca3cf286592575f1cd906a4131cf30c1c425f
Pilate Chinyengetere <pilatech6@gmail.com>
2023-01-02 18:02:55 -0800

    add strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+
+/**
+ * _strchr - Check is string s has char c
+ * @s: The string
+ * @c: The char
+ *
+ * Return: pointer or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+
+for (i = 0; i < sizeof(s); i++)
+{
+if (*(s + i) == c)
+return (s + i);
+}
+return (NULL);
+}
