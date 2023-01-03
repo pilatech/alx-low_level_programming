8bf9db2e164034c0689581e485fb648ac434b7a6
Pilate Chinyengetere <pilatech6@gmail.com>
2023-01-03 04:59:55 -0800

    add isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if it's an alphabet letter
+ * @c: the character to check
+ *
+ * Return: 1 if it is, else 0
+ */
+
+int _isalpha(int c)
+{
+if (((c > 64) && (c < 91)) || ((c > 96) && (c < 123)))
+	return (1);
+else
+	return (0);
+}
