96aec998777c62ad9bc08cc4c9414ea4cf7e55db
Pilate Chinyengetere <pilatech6@gmail.com>
2023-01-03 14:58:00 -0800

    add debug files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *main-tests function that prints if integer is positive or negative
- *@i: checked
- * Return: 0
- */
-void positive_or_negative(int i);
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+**Didnt i say am show you**
+=============================
+**There is a lot of string these time***
+--------------------------------------------
+- More pointers
+- More arrays
+- More string
