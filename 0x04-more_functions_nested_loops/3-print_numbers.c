fa62c206a81e6fbe3e875808b92f4542ce78e75f
Pilate Chinyengetere <pilatech6@gmail.com>
2023-01-03 12:08:21 -0800

    add print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+int n = 48;
+while (n < 58)
+{
+_putchar(n);
+n++;
+}
+_putchar(10);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ void _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
