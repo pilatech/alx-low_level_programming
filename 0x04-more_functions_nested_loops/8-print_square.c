8e8370ca5d693d45bba183e2a8a0cefc92490afb
Pilate Chinyengetere <pilatech6@gmail.com>
2023-01-03 13:15:01 -0800

    add print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: of size
+ *
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+int i = j = 0;
+if (size <= 0)
+{
+_putchar(10);
+}
+else
+{
+while (i < size)
+{
+while (j < size)
+{
+_putchar(35);
+j++;		}
+_putcha(10);
+i++;
+}
+}
+}
