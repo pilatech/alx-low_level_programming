9f735053e505032621b6af9e62b14331eea01697
Pilate Chinyengetere <pilatech6@gmail.com>
2023-01-03 05:38:34 -0800

    add abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - find absolute value of a number
+ * @num: the number
+ *
+ * Return: the number as absolute value
+ */
+
+int _abs(int num)
+{
+if (num > 0)
+	return (num);
+else
+	return (-num);
+}
