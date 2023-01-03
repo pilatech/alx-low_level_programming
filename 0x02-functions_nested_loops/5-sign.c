4d834e9c926309a9bc912bb87e1bfcf9b4f5527d
Pilate Chinyengetere <pilatech6@gmail.com>
2023-01-03 05:13:49 -0800

    add sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign for a number
+ * @n: the number
+ *
+ * Return: -1, 0 or 1
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(n);
+return (n);
+}
+}
