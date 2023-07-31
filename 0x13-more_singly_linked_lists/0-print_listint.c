85db5e128500219aa190324ee73f250681c02e48
Pilate Chinyengetere <pilatech6@gmail.com>
2023-07-31 09:41:52 +0200

    add task 0 file on 0x13

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - print the elements of the list.
+ * @h: pointer to the head of the list.
+ *
+ * Return: number of items in the list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
