307665e1dd9437073b35eb150e1a92a8488c7089
Pilate Chinyengetere <pilatech6@gmail.com>
2023-08-04 03:38:13 +0200

    add file n#0 of 0x14

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+unsigned int binary_to_uint(const char *b);
+unsigned int _pow(unsigned int num, unsigned int exp);
+void _putchar(int);
+void print_binary(unsigned long int n);
+
+#endif
