6c3f329b462a862cad38f4c62f40c9c896dc79a6
Pilate Chinyengetere <pilatech6@gmail.com>
2023-09-03 17:46:33 +0200

    add #1 of 0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - print binary representation of a number.
+ * @n: the number.
+ *
+ * Return: Nothing.
+ */
+void print_binary(unsigned long int n)
+{
+	long int pow;
+	long int h_square;
+
+	pow = 0;
+	h_square = 1;
+		while ((n >> pow) > 1)
+		{
+			h_square *= 2;
+			pow++;
+		}
+	while (h_square > 0)
+	{
+		if ((n >> pow) == 1)
+		{
+			_putchar('1');
+			n = n - h_square;
+		}
+		else
+			_putchar('0');
+		h_square >>= 1;	
+		pow--;	
+	}
+}
