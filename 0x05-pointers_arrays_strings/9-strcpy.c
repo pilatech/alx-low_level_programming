b4901101ed1f346ded0dc03fc9e615b39a79e60d
Pilate <pilatech6@gmail.com>
2023-09-24 11:01:07 +0200

    edit strcpy of 0x05

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *_strcpy- copies string
- *@dest:checked
- *@src: checked
- *Return: dest
+ * _strcpy- copies string to a given space
+ * @dest: where to copy from.
+ * @src: where to copy to
+ * Return: dest
  */
-
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (src[i])
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
