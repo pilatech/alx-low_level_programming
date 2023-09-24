2e23742c0b11e80bc61de7e17cc93cd2e5b63308
Pilate <pilatech6@gmail.com>
2023-09-24 09:24:52 +0200

    edit(again) puts2.c of 0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,15 @@ void puts2(char *str)
 
 	int i = 0;
 
-	while (*(str + i) != '\0')
+	if (str)
 	{
-		_putchar(str[i]);
-		i += 2;
+		while (*(str + i) != '\0')
+		{
+			_putchar(str[i]);
+			i += 2;
+		}
+		_putchar('\0');
+		_putchar('\n');
 	}
-	_putchar('\0');
-	_putchar('\n');
 }
 
