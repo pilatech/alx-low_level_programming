49ec332a25912ce80059bfa23cbd73752f4c8f9e
Pilate <pilatech6@gmail.com>
2023-10-12 12:36:23 +0200

    add #1 of 0x04

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 /**
- * print_dlistint - print the doubly-linked-list.
+ * dlistint_len - print the doubly-linked-list.
  * @h: starting of the list.
  *
  * Return: number of printed elements.
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 	dlistint_t *temp;
-	
+
 	if (h == NULL)
 		return (count);
 	if (h->next == NULL)
 	{
-		printf("%d\n", h->n);
 		count++;
 	}
 	else
@@ -23,7 +22,6 @@ size_t print_dlistint(const dlistint_t *h)
 
 		while (temp != NULL)
 		{
-			printf("%d\n", temp->n);
 			count++;
 			temp = temp->next;
 		}
