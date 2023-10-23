0d84704f1878c4212e710f3c14701c85bc1e870c
Pilate <pilatech6@gmail.com>
2023-10-23 03:26:05 +0200

    edit #101 of 0x13

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/pilatech/alx-low_level_programming/main/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://github.com/pilatech/alx-low_level_programming/blob/072d48a68d0447d6983ec3ad09c1215df24eae65/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
